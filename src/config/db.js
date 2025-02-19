d7237f9633abe2bc3034d7a266e5509cd47fc8dd
Lucas Eifler <lucass.eifler@gmail.comm>
2025-02-19 16:30:01 +0100

    created and connect mongoDB atlas to backend.

--- src/config/db.js
+++ src/config/db.js
@@ -2,19 +2,16 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    // Commenting on connecting to MongoDB to test the bankless server
-    // const conn = await mongoose.connect(process.env.MONGO_URI, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // });
-    console.log("MongoDB connection skipped for now.");
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-    // Instead of connecting to the bank, it just prints that the connection was ignored
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.bold);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error: ${error.message}`.red.bold);
     process.exit(1);
   }
 };
 
-// export default connectDB;
-export default () => {}; //Just an empty function so it doesn't break execution
+export default connectDB;
